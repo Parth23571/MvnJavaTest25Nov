eefec6018873c599d9c24b9488c62472580e573a
parth235 <pawal.pankaj@gmail.com>
2018-11-25 14:21:14 +0530

    Added 25Nov

--- src/test/java/com/MavenJavaProject/MavenJavaProject/Hello1Test.java
+++ src/test/java/com/MavenJavaProject/MavenJavaProject/Hello1Test.java
@@ -14,6 +14,7 @@ public class Hello1Test {
  System.setProperty("webdriver.chrome.driver","C:\\Browers\\chromedriver.exe");
  WebDriver dri=new ChromeDriver();
  dri.get("https://www.google.com");
+ 
  dri.manage().window().maximize();
   }
 }
